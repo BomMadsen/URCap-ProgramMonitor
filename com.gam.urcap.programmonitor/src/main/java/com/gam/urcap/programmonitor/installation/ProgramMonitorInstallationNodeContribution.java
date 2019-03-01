3c672bfac85eedbe8205a31ceac8358264fcce9a
jacobbsmadsen@gmail.com <jacobbsmadsen@gmail.com>
2019-03-01 15:59:21 +0100

    Added instance variable and due description

--- com.gam.urcap.programmonitor/src/main/java/com/gam/urcap/programmonitor/installation/ProgramMonitorInstallationNodeContribution.java
+++ com.gam.urcap.programmonitor/src/main/java/com/gam/urcap/programmonitor/installation/ProgramMonitorInstallationNodeContribution.java
@@ -6,9 +6,24 @@ import com.ur.urcap.api.domain.script.ScriptWriter;
 
 public class ProgramMonitorInstallationNodeContribution implements InstallationNodeContribution{
 
+	private final InstallationAPIProvider apiProvider;
+	private final ProgramMonitorInstallationNodeView view;
+	
+	/*****
+	 * BEWARE! 
+	 * This is an instance variable, and is not the typical approach for most URCaps. 
+	 * This is expressly used, since the Program Monitor should only be active for testing purposes. 
+	 * This setting is not persisted through reboot, reloading installations etc. 
+	 * For a more typical approach, where the value is saved, use the DataModel.
+	 * 
+	 * This variable indicates if Program Monitoring is set active for the time being. 
+	 */
+	private boolean ENABLE_PROGRAM_MONITORING = false;
+	
 	public ProgramMonitorInstallationNodeContribution(InstallationAPIProvider apiProvider,
 			ProgramMonitorInstallationNodeView view) {
-		// TODO Auto-generated constructor stub
+		this.apiProvider = apiProvider;
+		this.view = view;
 	}
 	
 	@Override

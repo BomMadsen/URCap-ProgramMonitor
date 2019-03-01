9c2a1b6f744ecc32a1e5b2d3050f8e0d5c156491
jacobbsmadsen@gmail.com <jacobbsmadsen@gmail.com>
2019-03-01 15:59:41 +0100

    Created a test class

---
+++ com.gam.urcap.programmonitor/src/main/java/com/gam/urcap/programmonitor/impl/TestersChoice.java
@@ -0,0 +1,15 @@
+package com.gam.urcap.programmonitor.impl;
+
+public class TestersChoice {
+
+	/*****
+	 * This is a test-class, used to validate designs throughout development. 
+	 * The class has no meaning to the general URCap. 
+	 * But it can be used to test various functionality without building and deploying the URCap.
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
